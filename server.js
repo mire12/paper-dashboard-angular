e130c57a9b20634cb9084a496088548c255d3d97
mire12 <miroo.koren@gmail.com>
2022-12-21 16:40:32 +0100

    update server.js to to be able to run on heroku

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 var cors = require('cors')
 const app = express();
 app.use(cors());
-app.use(express.static('./dist/paper-dashboard-angular'));
+app.use(express.static('./dist'));
 
 app.get('/*', function (req, res) {
-  res.sendFile('index.html', { root: 'dist/paper-dashboard-angular' }
+  res.sendFile('index.html', { root: 'dist' }
   );
 });
 
